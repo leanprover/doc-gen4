dbbe11da0a34026a1d9aad4d6c31bdba72ff99ca
Henrik Böving <hargonix@gmail.com>
2021-12-12 13:28:52 +0100

    chore: In file licensing

--- DocGen4/IncludeStr.lean
+++ DocGen4/IncludeStr.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 Henrik Böving. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Henrik Böving
+-/
 import Lean
 
 namespace DocGen4
