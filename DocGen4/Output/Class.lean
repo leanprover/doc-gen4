b777250338affd109bf8f92d2fe8ffe68b934499
Henrik Böving <hargonix@gmail.com>
2022-01-06 14:51:46 +0100

    feat: list instances of classes

--- DocGen4/Output/Class.lean
+++ DocGen4/Output/Class.lean
@@ -5,9 +5,22 @@ namespace DocGen4
 namespace Output
 
 open scoped DocGen4.Jsx
+open Lean
+
+def instanceToHtml (name : Name) : HtmlM Html := do
+  <li><a href={←declNameToLink name}>{name.toString}</a></li>
+
+def instancesToHtml (i : ClassInfo) : HtmlM Html := do
+  let instancesHtml ← i.instances.mapM instanceToHtml
+  return <details «class»="instances">
+        <summary>Instances</summary>
+        <ul>
+          [instancesHtml]
+        </ul>
+    </details>
 
 def classToHtml (i : ClassInfo) : HtmlM (Array Html) := do
-  structureToHtml i.toStructureInfo
+  (←structureToHtml i.toStructureInfo).push (←instancesToHtml i)
 
 end Output
 end DocGen4
