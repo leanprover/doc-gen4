de16febeb661be73d4e61a57bc3777e09857a6b6
Siddharth Bhat <siddu.druid@gmail.com>
2022-02-15 10:06:38 +0000

    Explain what root/url does.

--- Main.lean
+++ Main.lean
@@ -6,6 +6,7 @@ open DocGen4 Lean IO
 def main (args : List String) : IO Unit := do
   if args.isEmpty then
     IO.println "Usage: doc-gen4 root/url/ Module1 Module2 ..."
+    IO.println " 'root/url' generates resource paths (style.css, nav.js, etc.) as 'root/url/style.css', etc."
     IO.Process.exit 1
     return
   let root := args.head!
