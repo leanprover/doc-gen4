b87c1cc1ded9d0ffb38b8aeefb222b457eaf52ab
Henrik Böving <hargonix@gmail.com>
2022-02-20 13:45:18 +0100

    feat: Better equation Handling
    The equation content is now escaped, furthermore there is a limit
    for the length of an equation so we don't just fill everything with
    uselessly big equations nobody can read anyways.

--- DocGen4/Output/Definition.lean
+++ DocGen4/Output/Definition.lean
@@ -7,20 +7,35 @@ namespace Output
 open scoped DocGen4.Jsx
 open Lean Widget
 
+/- This is basically an arbitrary number that seems to work okay. -/
+def equationLimit : Nat := 200
+
 def equationToHtml (c : CodeWithInfos) : HtmlM Html := do
   pure <li «class»="equation">[←infoFormatToHtml c]</li>
 
 def equationsToHtml (i : DefinitionInfo) : HtmlM (Array Html) := do
   if let some eqs := i.equations then
     let equationsHtml ← eqs.mapM equationToHtml
-    pure #[
-      <details>
-        <summary>Equations</summary>
-        <ul «class»="equations">
-          [equationsHtml]
-        </ul>
-      </details>
-    ]
+    let filteredEquationsHtml := equationsHtml.filter (λ eq => eq.textLength < equationLimit)
+    if equationsHtml.size ≠ filteredEquationsHtml.size then
+      pure #[
+        <details>
+          <summary>Equations</summary>
+          <ul «class»="equations">
+            <li «class»="equation">One or more equations did not get rendered due to their size.</li>
+            [filteredEquationsHtml]
+          </ul>
+        </details>
+      ]
+    else
+      pure #[
+        <details>
+          <summary>Equations</summary>
+          <ul «class»="equations">
+            [filteredEquationsHtml]
+          </ul>
+        </details>
+      ]
   else
     pure #[]
 
