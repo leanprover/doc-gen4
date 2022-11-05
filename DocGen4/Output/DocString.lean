d62268b013b1d03fdafe96c1381c0effbdfd3ad1
Alex J. Best <alex.j.best@gmail.com>
2022-11-05 18:46:20 +0100

    better

--- DocGen4/Output/DocString.lean
+++ DocGen4/Output/DocString.lean
@@ -188,7 +188,9 @@ partial def modifyElement (element : Element) : HtmlM Element :=
     else if name = "a" then
       extendAnchor el
     -- auto link for inline <code></code>
-    else if name = "code" ∧ attrs.find? "class" = "language-lean" then
+    else if name = "code" ∧
+      -- don't linkify code blocks explicitly tagged with a language other than lean
+      ((¬ attrs.contains "class") ∨ (((attrs.find? "class").getD "").splitOn).filter (fun s => s.startsWith "language-" ∧ s ≠ "language-lean") = []) then
       autoLink el
     -- recursively modify
     else
