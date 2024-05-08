b91fea210b7b6b451f19c6344d1f82765b9607af
Henrik Böving <hargonix@gmail.com>
2024-05-08 07:54:33 +0200

    chore: fix CI

--- test_docs.sh
+++ test_docs.sh
@@ -1,5 +1,5 @@
 # Arguments:
-# $1 : path to std4 from working directory
+# $1 : path to batteries from working directory
 # $2 : path to doc-gen4 from working directory
 
 set -e
@@ -7,7 +7,7 @@ set -x
 
 # generate the docs
 cd "$1"
-sed -i "s|from git \"https://github.com/leanprover/doc-gen4\" @ \"main\"| from \"..\" / \"doc-gen4\" with NameMap.empty|" lakefile.lean
+sed -i "s|from git \"https://github.com/leanprover/doc-gen4\" @ \"main\"| from \"..\" / \"doc-gen4\"|" lakefile.lean
 
 lake -R -Kdoc=on update
-lake -R -Kdoc=on build Std:docs
+lake -R -Kdoc=on build Batteries:docs
