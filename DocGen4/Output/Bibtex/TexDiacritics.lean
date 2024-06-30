08bf731148931343dee5fe0350435dc5efdd59e5
acmepjz <acme_pjz@hotmail.com>
2024-06-30 08:32:15 +0800

    chore: fix the error message propagation

--- DocGen4/Output/Bibtex/TexDiacritics.lean
+++ DocGen4/Output/Bibtex/TexDiacritics.lean
@@ -24,30 +24,28 @@ def normalChar : Parsec Char := satisfy fun c =>
 /-- Match at least one normal characters which is not the beginning of TeX command. -/
 def normalChars : Parsec String := many1Chars normalChar
 
-/-- Match a TeX command (with `\` removed) starting with a letter.
-Return value will add the `\` back. -/
-def texCommandAux : Parsec String := attempt do
-  let s ← many1Chars asciiLetter
-  match ← peek? with
-  | .some c =>
-    match c with
-    | '*' =>
-      skip
-      return "\\" ++ s ++ toString c ++ (← ws')
-    | _ =>
-      return "\\" ++ s ++ (← ws')
-  | .none =>
-    return "\\" ++ s
-
 /-- Match a TeX command starting with `\`, potentially with trailing whitespaces. -/
-def texCommand : Parsec String :=
-  pchar '\\' *> (texCommandAux <|> ("\\".push <$> anyChar))
+def texCommand : Parsec String := pchar '\\' *> attempt do
+  let s ← manyChars asciiLetter
+  if s.isEmpty then
+    return "\\" ++ toString (← anyChar) ++ (← ws')
+  else
+    match ← peek? with
+    | .some c =>
+      match c with
+      | '*' =>
+        skip
+        return "\\" ++ s ++ toString c ++ (← ws')
+      | _ =>
+        return "\\" ++ s ++ (← ws')
+    | .none =>
+      return "\\" ++ s
 
 /-- Similar to `texCommand` but it excludes some commands. -/
 def texCommand' (exclude : Array String) : Parsec String := attempt do
   let s ← texCommand
   match exclude.find? (· == s.trim) with
-  | .some _ => fail ""
+  | .some _ => fail s!"{s.trim} is not allowed"
   | .none => return s
 
 /-- Match a sequence starting with `{` and ending with `}`. -/
@@ -58,20 +56,46 @@ def bracedContent (p : Parsec String) : Parsec String :=
 def bracedContent' (p : Parsec String) : Parsec String :=
   pchar '{' *> p <* pchar '}'
 
-partial def mathContentAux2 : Parsec String := attempt do
-  let ret : Array String ← many <|
-    bracedContent mathContentAux2 <|>
-    texCommand' #["\\(", "\\)", "\\[", "\\]"] <|>
-    normalChars
-  return String.join ret.toList
+partial def manyOptions {α} (p : Parsec (Option α)) (acc : Array α := #[]) :
+    Parsec (Array α) := fun it =>
+  match p it with
+  | .success it ret =>
+    match ret with
+    | .some ret => manyOptions p (acc.push ret) it
+    | .none => .success it acc
+  | .error it err => .error it err
+
+partial def mathContentAux2 : Parsec String := do
+  let doOne : Parsec (Option String) := fun it =>
+    if it.hasNext then
+      match it.curr with
+      | '{' => (.some <$> bracedContent mathContentAux2) it
+      | '\\' =>
+        match texCommand' #["\\(", "\\)", "\\[", "\\]"] it with
+        | .success it ret => .success it (.some ret)
+        | .error _ _ => .success it .none
+      | '}' | '$' => .success it .none
+      | _ => (.some <$> normalChars) it
+    else
+      .success it .none
+  return String.join (← manyOptions doOne).toList
 
 def mathContentAux (beginning ending dollar : String) : Parsec String :=
   pstring beginning *> ((dollar ++ · ++ dollar) <$> mathContentAux2) <* pstring ending
 
 /-- Match a math content. -/
-def mathContent : Parsec String :=
-  mathContentAux "\\[" "\\]" "$$" <|> mathContentAux "\\(" "\\)" "$" <|>
-  mathContentAux "$$" "$$" "$$" <|> mathContentAux "$" "$" "$"
+def mathContent : Parsec (Option String) := fun it =>
+  let substr := it.extract (it.forward 2)
+  if substr = "\\[" then
+    (.some <$> mathContentAux "\\[" "\\]" "$$") it
+  else if substr = "\\(" then
+    (.some <$> mathContentAux "\\(" "\\)" "$") it
+  else if substr = "$$" then
+    (.some <$> mathContentAux "$$" "$$" "$$") it
+  else if it.curr = '$' then
+    (.some <$> mathContentAux "$" "$" "$") it
+  else
+    .success it .none
 
 /-- Match a TeX command for diacritics, return the corresponding UTF-8 string. -/
 def texDiacriticsCommand : Parsec String := attempt do
@@ -110,22 +134,42 @@ def texDiacriticsCommand : Parsec String := attempt do
   | _ => fail s!"unsupported command: {cmd}"
 
 /-- Convert all TeX commands for diacritics into UTF-8 characters,
-and stop on any other TeX commands which are not in math environment. -/
-partial def texDiacritics : Parsec String := attempt do
-  let ret : Array String ← many <|
-    bracedContent texDiacritics <|>
-    mathContent <|>
-    texDiacriticsCommand <|>
-    normalChars
-  return String.join ret.toList
+and error on any other TeX commands which are not in math environment. -/
+partial def texDiacritics : Parsec String := do
+  let doOne : Parsec (Option String) := fun it =>
+    if it.hasNext then
+      match mathContent it with
+      | .success it ret =>
+        match ret with
+        | .some ret => .success it (.some ret)
+        | .none =>
+          match it.curr with
+          | '{' => (.some <$> bracedContent texDiacritics) it
+          | '\\' => (.some <$> texDiacriticsCommand) it
+          | '}' => .success it .none
+          | _ => (.some <$> normalChars) it
+      | .error it err => .error it err
+    else
+      .success it .none
+  return String.join (← manyOptions doOne).toList
 
 /-- Remove all braces except for those in math environment,
-any stop on any TeX commands which are not in math environment. -/
-partial def removeBraces : Parsec String := attempt do
-  let ret : Array String ← many <|
-    bracedContent' removeBraces <|>
-    mathContent <|>
-    normalChars
-  return String.join ret.toList
+and error on any TeX commands which are not in math environment. -/
+partial def removeBraces : Parsec String := do
+  let doOne : Parsec (Option String) := fun it =>
+    if it.hasNext then
+      match mathContent it with
+      | .success it ret =>
+        match ret with
+        | .some ret => .success it (.some ret)
+        | .none =>
+          match it.curr with
+          | '{' => (.some <$> bracedContent' removeBraces) it
+          | '}' => .success it .none
+          | _ => (.some <$> normalChars) it
+      | .error it err => .error it err
+    else
+      .success it .none
+  return String.join (← manyOptions doOne).toList
 
 end DocGen4.Bibtex
