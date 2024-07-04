ca743c16882e39d8adc388f7bc7aac67aa52580f
Henrik Böving <hargonix@gmail.com>
2024-07-04 23:03:38 +0200

    chore: fix CI

--- test_docs.sh
+++ test_docs.sh
@@ -7,7 +7,7 @@ set -x
 
 # generate the docs
 cd "$1"
-sed -i "s|from git \"https://github.com/leanprover/doc-gen4\" @ \"main\"| from \"..\" / \"doc-gen4\"|" lakefile.lean
+echo 'require «doc-gen4» from ".." / "doc-gen4"' >> lakefile.lean
 
-lake -R -Kdoc=on update
-lake -R -Kdoc=on build Batteries:docs
+lake update doc-gen4
+lake build Batteries:docs
