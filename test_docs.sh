98795eeeb6e9d701eb60c02fb9e012d067dfa790
Kim Morrison <kim@tqft.net>
2024-11-01 16:55:47 +1100

    Batteries uses a toml now

--- test_docs.sh
+++ test_docs.sh
@@ -1,13 +1,16 @@
 # Arguments:
 # $1 : path to batteries from working directory
-# $2 : path to doc-gen4 from working directory
 
 set -e
 set -x
 
 # generate the docs
 cd "$1"
-echo 'require «doc-gen4» from ".." / "doc-gen4"' >> lakefile.lean
+cat >> lakefile.lean << 'EOL'
+[[require]]
+name = "«doc-gen4»"
+path = "../doc-gen4"
+EOL
 
 lake update doc-gen4
 lake build Batteries:docs
