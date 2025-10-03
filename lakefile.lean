9e0d1f768271ba888d24529fe5afa89b967df623
Markus Himmel <markus@lean-fro.org>
2025-10-03 07:41:52 +0200

    lake update

--- lakefile.lean
+++ lakefile.lean
@@ -15,7 +15,7 @@ require MD4Lean from git
   "https://github.com/acmepjz/md4lean" @ "main"
 
 require BibtexQuery from git
-  "https://github.com/dupuisf/BibtexQuery" @ "master"
+  "https://github.com/TwoFx/BibtexQuery" @ "nightly-testing"
 
 require «UnicodeBasic» from git
   "https://github.com/fgdorais/lean4-unicode-basic" @ "main"
