9eac5158f8e4e0234f43c21b411ee81bc99b1b9d
David Thrane Christiansen <david@davidchristiansen.dk>
2026-02-20 12:01:04 +0100

    doc: more informative comment

--- DocGen4/Output/Structure.lean
+++ DocGen4/Output/Structure.lean
@@ -41,11 +41,31 @@ def fieldToHtml (structName : Name) (f : Process.FieldInfo) : HtmlM Html := do
   else
     -- In some cases, multiple inheritance leads to the generation of projection functions for
     -- inherited fields. In these cases, the generated projection should be a valid link target.
+    --
+    -- Normally, Lean does not generate a projection for each inherited field. For instance, given:
+    --     structure A where
+    --       x : Nat
+    --     structure B extends A where
+    --       y : String
+    --
+    -- Lean does not generate `B.x`. Instead, the `x` projection of a term of type `B` is
+    -- implemented by a synthesized term `B.toA.x`. If a user defines `B.x := B.toA.x`, then
+    -- documentation should link to this new definition. However, the structure elaborator does
+    -- sometimes create the function, such as in this situation:
+    --     structure A where
+    --       x : Nat
+    --     structure B  extends A
+    --     structure C extends A where
+    --       y : Unit
+    --     structure D extends B, C
+    -- where D.y is in fact generated.
+    --
     -- However, just checking for the field name as a suffix of the structure name is not
-    -- sufficient, because a user could also define that function, leading to duplicate ids or
-    -- incorrect link targets. The solution is to check whether the name matches the pattern, _and_
-    -- whether it's defined in the source code range of the structure (the structure elaborator
-    -- associates them with the entry in the `extends` clause).
+    -- sufficient, because when Lean doesn't generate a projection, users are free to define
+    -- functions with that name, which could lead to duplicate ids or incorrect link targets. The
+    -- solution is to check whether the name matches the pattern, _and_ whether it's defined in the
+    -- source code range of the structure (the structure elaborator associates them with the entry
+    -- in the `extends` clause).
     let projName := structName ++ getShort' f.name
     let inner :=
       <div class="structure_field_info">
